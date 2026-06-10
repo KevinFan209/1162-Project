07527967ccf033358e61e4167a51704556af2ede
yhsuan27 <yyhsuan27@gmail.com>
2026-06-10 17:40:25 +0800

    Delete 新UI設計 directory

--- 新UI設計/UI新版/js/lobby.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const sideMenu = document.getElementById('sideMenu');
-    const overlay = document.getElementById('overlay');
-    sideMenu.classList.toggle('open');
-    overlay.classList.toggle('open');
-}
